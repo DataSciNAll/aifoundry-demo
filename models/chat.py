81eb451751e6b5c00f3cd0b9786776636df9fe5e
Jason Virtue <virtuej@gmail.com>
2025-02-15 19:49:21 +0000

    Add Parm

--- models/chat.py
+++ models/chat.py
@@ -1,7 +1,7 @@
 from azure.ai.projects import AIProjectClient
 from azure.identity import DefaultAzureCredential
 
-project_connection_string = "eastus2.api.azureml.ms;597966d1-829f-417e-9950-8189061ec09c;rg-dantaylo-e2e-demo2;dantaylo-e2e-demo2"
+project_connection_string = "apihere"
 
 project = AIProjectClient.from_connection_string(
     conn_str=project_connection_string,
